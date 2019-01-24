5d250cff5429c422a64fdaacb070e349a1f724da
parnupat.j <dainokung2535@gmail.com>
2019-01-24 17:50:13 +0700

    init app

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 console.log('Hello World !!')
 const fs = require('fs');
 const os = require('os');
-
-var user = os.userInfo();
-console.log(user)
-
-fs.appendFile('greetings.txt',`Hello ${user.username}!`,function(err){
-    if(err){
-        console.log('Unable to wire to file');
-    }
-});
+const notes = require('./notes')
+const _ = require('lodash')
+// console.log(_.isString('true'))
+// console.log('Result: ', notes.add(9,2));
+var filteredArray = _.uniq(['a',1,'a',1,2,3,4]);
+console.log(filteredArray)
+// var res = notes.addNote();
+// console.log(res)
+// var user = os.userInfo();
+// fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`,function(err){
+//     if(err){
+//         console.log('Unable to wire to file');
+//     }
+// });
