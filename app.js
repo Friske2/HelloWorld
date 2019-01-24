126b622c28f54247905f065b8ebc57a7cfdc97b7
parnupat.j <dainokung2535@gmail.com>
2019-01-24 10:56:15 +0700

    use fs and os

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('Hello World !!')
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+console.log(user)
+
+fs.appendFile('greetings.txt',`Hello ${user.username}!`,function(err){
+    if(err){
+        console.log('Unable to wire to file');
+    }
+});
