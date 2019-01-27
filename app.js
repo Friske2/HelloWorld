47f7dd377dd72fc2ce6a0651cad877b019170ff1
dainokung <dainokung2535@gmail.com>
2019-01-27 12:19:18 +0700

    crud app

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
-console.log('Hello World !!')
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes')
 const _ = require('lodash')
-// console.log(_.isString('true'))
-// console.log('Result: ', notes.add(9,2));
-var filteredArray = _.uniq(['a',1,'a',1,2,3,4]);
-console.log(filteredArray)
-// var res = notes.addNote();
-// console.log(res)
-// var user = os.userInfo();
-// fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`,function(err){
-//     if(err){
-//         console.log('Unable to wire to file');
-//     }
-// });
+const yargs = require('yargs')
+const titleOptions = {
+    describe : 'Title of note',
+    demand: true,
+    alias: 't'
+}
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
+const argv = yargs.command('add','Add a new note',{
+    title:titleOptions,
+    body:bodyOptions
+})
+.command('list','List all notes')
+.command('read','Read a note',{
+    title: titleOptions
+})
+.command('remove','Remove a note',{
+    title: titleOptions
+})
+.help()
+.argv;
+var command = argv._[0];
+console.log('Command : ',command);
+if(command === 'add'){
+    var note = notes.addNote(argv.title,argv.body)
+    if(note) {
+        console.log('Note created')
+        notes.logNote(note);
+    }
+    else {
+        console.log('Note title taken')
+    }
+}
+else if(command === 'list'){
+    console.log('Listenig all notes');
+    var allNotes = notes.getAll()
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach((note)=>notes.logNote(note)) 
+}
+else if(command === 'read'){
+    console.log('Reading note')
+    var note = notes.getNote(argv.title)
+    if(note){
+        console.log('Note found');
+        notes.logNote(note);
+    }
+    else{
+        console.log('Note not found')
+    }
+}
+else if(command === 'remove'){
+    var noteRemove = notes.removeNote(argv.title)
+    var message = noteRemove ? 'Note was removed' : 'Note not found';
+    console.log(message)
+}
+else {
+    console.log('Command not recognized')
+}
